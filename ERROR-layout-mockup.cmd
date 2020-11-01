b3c0c2fe4fa854de963c770144d8e97303fda183
Feldwor <BoQsc@users.noreply.github.com>
2020-11-01 20:55:35 +0200

    Create ERROR-layout-mockup.cmd

---
+++ ERROR-layout-mockup.cmd
@@ -0,0 +1,92 @@
+@echo off
+
+cls
+echo.                                         
+echo                   Windows sistemoje            Linux sistemoje      
+echo               ------------------------    -------------------------    
+echo                curl vaido.world ^| cmd        GET vaido.world ^| bash   
+echo               ------------------------     -------------------------    
+echo                         ^^                                            
+echo                         ^|                                            
+echo                         ^|                                            
+echo                         ^|                                            
+echo                         ^|               
+echo.                               
+   
+@echo off
+
+cls
+echo                                         ^|^|
+echo                   Windows sistemoje     ^|^|         Linux sistemoje      
+echo               ------------------------  ^|^|    -------------------------    
+echo                curl vaido.world ^| cmd   ^|^|      GET vaido.world ^| bash   
+echo               ------------------------  ^|^|    -------------------------    
+echo                         ^^               ^|^|                              
+echo                         ^|               ^|^|                              
+echo                         ^|               ^|^|                              
+echo                         ^|               ^|^|                              
+echo                         ^|               ^|^| 
+echo.                               
+   
+@echo off
+
+cls
+echo                                         ^|
+echo                 (Windows sistemoje)     ^|        (Linux sistemoje)      
+echo               ------------------------  ^|    -----------------------    
+echo                curl vaido.world ^| cmd   ^|      GET vaido.world ^| bash   
+echo               ------------------------  ^|    -----------------------    
+echo                        ^^                ^|               ^^                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo ............................................
+
+@echo off
+
+cls
+echo                                         ^|
+echo                 (Windows sistemoje)     ^|        (Linux sistemoje)      
+echo               ------------------------  ^|    -----------------------    
+echo                curl vaido.world ^| cmd   ^|      GET vaido.world ^| bash   
+echo               ------------------------  ^|    -----------------------    
+echo                        ^^                ^|               ^^                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo                        ^|                ^|               ^|                
+echo .........................................................
+
+@echo off
+
+cls
+echo             (Windows sistemoje)              (Linux sistemoje)      
+echo           ------------------------       -----------------------    
+echo            curl vaido.world ^| cmd         GET vaido.world ^| bash   
+echo           ------------------------       -----------------------    
+echo                    ^^                               ^^                
+echo                    ^|                               ^|                
+echo                    ^|                               ^|                
+echo                    ^|                               ^|                
+echo                    ^|                               ^|                
+echo                    ^|                               ^|                
+
+
+@echo off
+
+
+echo  (Windows sistemoje)      (Windows sistemoje)
+echo  ------------------       ------------------
+echo  vaido.world ^| cmd       vaido.world ^| cmd 
+echo  ------------------       ------------------
+echo           ^^                       ^^
+echo           ^|                       ^|
+echo           ^|                       ^|
+echo           ^|                       ^|
+echo           ^|                       ^|
+echo           ^|                       ^|
+
+pause
